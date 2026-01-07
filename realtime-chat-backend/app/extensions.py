f08b92be3a185e2c71f4b67c4df435302b612896
elgenio <rickentchabet@gmail.com>
2026-01-08 00:05:05 +0100

    edit run.py and extensions.py

--- realtime-chat-backend/app/extensions.py
+++ realtime-chat-backend/app/extensions.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import JWTManager
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 
 def init_extensions(app):
     db.init_app(app)
